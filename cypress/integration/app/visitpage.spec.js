af76d14e351ef2e9986e449cf8c05f81d941d648
Gurveer Kaur <gurveer.kaur.aulakh@gmail.com>
2021-02-11 08:45:30 -0800

    Cypress and Jest Setup
    Jest and Cypress Setup

--- cypress/integration/app/visitpage.spec.js
+++ cypress/integration/app/visitpage.spec.js
@@ -4,7 +4,6 @@ context("Assertions", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
   });
-
   describe("Page Opens Test", () => {
     it('finds the content "type"', () => {
       cy.contains("Learn React");
