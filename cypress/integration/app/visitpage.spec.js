d9a867147989e7b84979d3929ed9d8a53119d6c1
Little Munchkin Daycare <appdev@littlemunchkindaycare.ca>
2021-02-11 08:35:21 -0800

    Jest and Cypress Setup
    Jest and Cypress Setup

---
+++ cypress/integration/app/visitpage.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+context("Assertions", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  describe("Page Opens Test", () => {
+    it('finds the content "type"', () => {
+      cy.contains("Learn React");
+    });
+  });
+});
