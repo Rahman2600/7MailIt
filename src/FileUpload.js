4c7d18b92887ac6d7429a3b5161ab95600f7c09e
rahman2600 <abdurrahmanmudasiru@yahoo.com>
2021-02-20 14:47:53 +0100

    Refactor file upload to it's component use bootstrap alert to handle upload error cases

---
+++ src/FileUpload.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+class FileUpload extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { selectedFile: '', error: null }
+        this.errors = Object.freeze({
+            WRONG_FILE_TYPE:   "Wrong template file type. Upload a .doc or .docx file",
+            UPLOAD_FAIL:  "Upload Failure",
+            NO_FILE: "Please upload an Email template document"
+        });
+        this.onFileChange = this.onFileChange.bind(this);
+        this.onFileUpload = this.onFileUpload.bind(this);
+    }
+
+    render() {
+        return (
+            <div>
+                <button className="btn btn-light btn-block mt-5" onClick={this.onFileUpload}> Upload New Template </button>
+                {this.state.error != null ? 
+                    <div class="alert alert-danger" role="alert">
+                        {`Error: ${this.state.error}`} 
+                    </div>
+                    :
+                    <div></div>
+                }
+                <form>
+                    <div className="form-group">
+                        <input type="file" className="form-control-file" id="file" onChange={this.onFileChange}/>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+
+    onFileChange(event) {
+        this.setState({ selectedFile: event.target.files[0] });
+    }
+
+    onFileUpload() {
+        if (this.state.selectedFile === '') {
+            this.setState({error: this.errors.NO_FILE });
+            return;
+        }
+        var allowedExtensions = /(\.doc|\.docx)$/i;
+        var fileInput = this.state.selectedFile;
+        var filePath = fileInput.name;
+        console.log(filePath);
+        if(!allowedExtensions.exec(filePath)){
+            this.setState({error: this.errors.WRONG_FILE_TYPE });
+            // alert('Please upload file having extensions .doc or docx only.');
+            //fileInput.value = '';
+            return false;
+        }
+
+    }
+}
+
+export default FileUpload;
--- src/HomePageRight.js
+++ src/HomePageRight.js
@@ -1,11 +1,9 @@
 import React from "react";
+import FileUpload from "./FileUpload";
 
 class HomePageRight extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { selectedFile: '' }
-        this.onFileChange = this.onFileChange.bind(this);
-        this.onFileUpload = this.onFileUpload.bind(this);
     }
 
     render() {
@@ -13,32 +11,9 @@ class HomePageRight extends React.Component {
              <div className="right-component">
                 <button className="btn btn-light mt-5 ml-5 mr-5 mb-5 float-right"> Log out </button>
                 <button className="btn btn-light btn-block mt-5"> View Job Logs </button>
-                <form>
-                    <div className="form-group">
-                        <input type="file" className="form-control-file" id="file" onChange={this.onFileChange}/>
-                    </div>
-                </form>
-                <button className="btn btn-light btn-block mt-5" onClick={this.onFileUpload}> Upload New Template </button>
+                <FileUpload />
             </div> 
         )
-    }
-
-    onFileChange(event) {
-        this.setState({ selectedFile: event.target.files[0] });
-    }
-
-    onFileUpload() {
-        console.log(this.state.selectedFile);
-        var allowedExtensions = /(\.doc|\.docx)$/i;
-        var fileInput = this.state.selectedFile;
-        var filePath = fileInput.name;
-        console.log(filePath);
-        if(!allowedExtensions.exec(filePath)){
-            alert('Please upload file having extensions .doc or docx only.');
-            fileInput.value = '';
-            return false;
-        }
-
     }
 }
 
