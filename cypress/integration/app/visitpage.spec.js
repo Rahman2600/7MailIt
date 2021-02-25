36d6b295ac37d68237aa4ab89ab05e29173a3c00
mderose123 <matthew.david.derose@gmail.com>
2021-02-25 15:32:22 -0500

    Adjusted the cyprus test framework again

--- cypress/integration/app/visitpage.spec.js
+++ cypress/integration/app/visitpage.spec.js
@@ -1,12 +1,12 @@
 /// <reference types="cypress" />
 
-// context("Assertions", () => {
-//   beforeEach(() => {
-//     cy.visit("http://localhost:3000/");
-//   });
-//   describe("Page Opens Test", () => {
-//     it('finds the content "type"', () => {
-//       //cy.contains("Learn React");
-//     });
-//   });
-// });
+context("Assertions", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+  describe("Page Opens Test", () => {
+    it('finds the content "type"', () => {
+      //cy.contains("Learn React");
+    });
+  });
+});
