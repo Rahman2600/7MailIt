b5e42f351cbbdf3ba360f1381e414c614f4c6896
gurveer90 <>
2021-02-25 16:49:12 -0800

    Test case for sign-in page

--- cypress/integration/app/visitpage.spec.js
+++ cypress/integration/app/visitpage.spec.js
@@ -4,9 +4,9 @@ context("Assertions", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
   });
-  describe("Page Opens Test", () => {
+  describe("Page Opens Sign-In Page", () => {
     it('finds the content "type"', () => {
-      //cy.contains("Learn React");
+      cy.contains("Sign in");
     });
   });
 });
