edcb11b56b716531f20728462b2bd4ff7c450970
Gurveer90 <41240660+Gurveer90@users.noreply.github.com>
2021-02-28 10:05:19 -0800

    tests login

--- cypress/integration/app/visitpage.spec.js
+++ cypress/integration/app/visitpage.spec.js
@@ -1,12 +1,52 @@
 /// <reference types="cypress" />
 
+const { ThreeDRotation } = require("@material-ui/icons");
+
 context("Assertions", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
   });
-  describe("Page Opens Sign-In Page", () => {
-    it('finds the content "type"', () => {
-      cy.contains("Sign in");
+  describe("Page Opens Test", () => {
+    it('login success and navigates to first template', () => {
+
+      // login
+      cy.get('#email')
+        .should('be.visible')
+        .type('mountainSasquatch00@gmail.com');
+      cy.get('#password')
+        .should('be.visible')
+        .type('teamMailIt!');
+      cy.get('button[type="submit"]')
+        .should('be.visible')
+        .click();
+
+      cy.wait(5000);
+
+      // click ready
+      cy.get('a[href*="/campaignPage/3fda23114tdf5"]').first().click()
+      cy.wait(5000);
+
+      cy.get('img[class="img-rounded"]')
+        .should('be.visible');
     });
   });
+  it('login fail', () => {
+
+    // login
+    cy.get('#email')
+      .should('be.visible')
+      .type('mountainSauatch00@gmail.com');
+    cy.get('#password')
+      .should('be.visible')
+      .type('teamMailIt!');
+    cy.get('button[type="submit"]')
+      .should('be.visible')
+      .click();
+
+    cy.wait(5000);
+
+    cy.url().should('eq', 'http://localhost:3000/');
+
+  });
 });
+
