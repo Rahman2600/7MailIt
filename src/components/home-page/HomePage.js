852fdc18f911b490f80ef33f68f9cc92e523b543
mderose123 <matthew.david.derose@gmail.com>
2021-03-23 08:02:24 -0400

    mderose123 - AFixed another merge issue

--- src/components/home-page/HomePage.js
+++ src/components/home-page/HomePage.js
@@ -6,8 +6,11 @@ import "../../styles/HomePage.css"
 class HomePage extends React.Component {
 	constructor(props) {
 		super(props);
+		this.onUploadSuccess = this.onUploadSuccess.bind(this);
+		this.key = 0;
 	}
 	render() {
+		this.key += 1;
 		return (
 			<div className="scroll container-fluid">
 				<TemplateLogTable key={this.key}/>
