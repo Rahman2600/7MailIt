4e237d501d3a6fc82dbfb70f2b135e06458ce516
gurveer90 <>
2021-04-10 06:54:15 -0700

    email send fail test

--- cypress/integration/app/sendemail.spec.js
+++ cypress/integration/app/sendemail.spec.js
@@ -21,7 +21,7 @@ context("Assertions", () => {
         .should('be.visible')
         .click();
 
-      cy.wait(5000);
+      cy.wait(10000);
 
       // click ready
       cy.get('table').contains('td', "donotremove.docx").siblings().contains('a', 'Start').click();
@@ -31,7 +31,7 @@ context("Assertions", () => {
       cy.get('#email-address')
         .should('be.visible')
         .type('gurveer.kaur.aulakh@gmail.com');
-        cy.wait(5000);
+      cy.wait(10000);
 
       cy.get('#subject-line')
         .should('be.visible')
@@ -53,6 +53,51 @@ context("Assertions", () => {
         .should('be.visible');
 
     });
+
+    it('login success and send email fail test', () => {
+
+      // login
+      cy.get('#email')
+        .should('be.visible')
+        .type('mountainSasquatch00@gmail.com');
+      cy.get('#password')
+        .should('be.visible')
+        .type('teamMailIt!');
+      cy.get('button[type="submit"]')
+        .should('be.visible')
+        .click();
+
+      cy.wait(10000);
+
+      // click ready
+      cy.get('table').contains('td', "donotremove.docx").siblings().contains('a', 'Start').click();
+      cy.wait(5000);
+
+      //single email test
+      cy.get('#email-address')
+        .should('be.visible')
+        .type('gurveer.kaur.aulakh@gmail.com');
+      cy.wait(5000);
+
+ 
+      cy.get('input[aria-label="name"]')
+        .should('be.visible')
+        .type('abc');
+      cy.get('input[aria-label="AMOUNT"]')
+        .should('be.visible')
+        .type('2000');
+      cy.get('input[aria-label="PROMO_CODE"]')
+        .should('be.visible')
+        .type('def');
+      cy.get('button[id="button1"]')
+        .should('be.visible')
+        .click();
+      cy.wait(5000);
+      cy.get('#emailSentFailed')
+        .should('be.visible');
+
+    });
+
   });
 
 });
