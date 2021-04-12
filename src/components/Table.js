210e09b6015fea0383eb5c0de00927f27dd20e82
rahman2600 <abdurrahmanmudasiru@yahoo.com>
2021-04-12 21:45:55 +0100

    remove console.logs

--- src/components/Table.js
+++ src/components/Table.js
@@ -120,9 +120,6 @@ class Table extends React.Component {
             b = b.truncatedContent.fullVersion.toLowerCase();
         }
     
-        console.log(a);
-        console.log(b);
-    
         if (a > b) return 1;
         else if (a < b) return -1;
         else return 0;
